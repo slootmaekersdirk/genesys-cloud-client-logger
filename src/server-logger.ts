e703b6da309b44def5827cc55f0c332ec10a5079
Dirk Slootmaekers <slootmaekersdirk@gmail.com>
2023-01-30 11:20:21 +0100

    removed eventlistener

--- src/server-logger.ts
+++ src/server-logger.ts
@@ -1,9 +1,15 @@
-import stringify from 'safe-json-stringify';
+import stringify from "safe-json-stringify";
 
-import { Logger } from './logger';
-import { LogLevel, ITrace, ILogBufferItem, ILogMessage, ISendLogRequest } from './interfaces';
-import { calculateLogMessageSize, deepClone } from './utils';
-import { getOrCreateLogUploader, LogUploader } from './log-uploader';
+import { Logger } from "./logger";
+import {
+  LogLevel,
+  ITrace,
+  ILogBufferItem,
+  ILogMessage,
+  ISendLogRequest,
+} from "./interfaces";
+import { calculateLogMessageSize, deepClone } from "./utils";
+import { getOrCreateLogUploader, LogUploader } from "./log-uploader";
 
 const MAX_LOG_SIZE = 14500;
 const DEFAULT_UPLOAD_DEBOUNCE = 4000;
@@ -16,38 +22,44 @@ export class ServerLogger {
   private debounceTimer: any = null;
   private logUploader: LogUploader;
 
-  constructor (logger: Logger) {
+  constructor(logger: Logger) {
     this.logger = logger;
 
     /* if we have all the needed config options, set this up */
-    if (
-      !logger.config.url ||
-      !logger.config.appVersion
-    ) {
-      const errMessage = 'Missing `url` and/or `appVersion` config options to set up server logging. ' +
-        'Not sending logs to server for this logger instance';
-      this.logger.error(errMessage, { providedConfig: logger.config }, { skipServer: true });
+    if (!logger.config.url || !logger.config.appVersion) {
+      const errMessage =
+        "Missing `url` and/or `appVersion` config options to set up server logging. " +
+        "Not sending logs to server for this logger instance";
+      this.logger.error(
+        errMessage,
+        { providedConfig: logger.config },
+        { skipServer: true }
+      );
       throw new Error(errMessage);
     }
 
     this.isInitialized = true;
-    this.debounceLogUploadTime = logger.config.uploadDebounceTime || DEFAULT_UPLOAD_DEBOUNCE;
-    this.logUploader = getOrCreateLogUploader(logger.config.url, logger.config.debugMode);
+    this.debounceLogUploadTime =
+      logger.config.uploadDebounceTime || DEFAULT_UPLOAD_DEBOUNCE;
+    this.logUploader = getOrCreateLogUploader(
+      logger.config.url,
+      logger.config.debugMode
+    );
 
-    window.addEventListener('unload', this.sendAllLogsInstantly.bind(this));
+    //DSL window.addEventListener('unload', this.sendAllLogsInstantly.bind(this));
 
     /* when we stop server logging, we need to clear everything out */
-    this.logger.on('onStop', (_reason) => {
-      this.debug('`onStop` received. Clearing logBuffer and sendQueue', {
+    this.logger.on("onStop", (_reason) => {
+      this.debug("`onStop` received. Clearing logBuffer and sendQueue", {
         logBuffer: this.logBuffer,
-        sendQueue: this.logUploader.sendQueue
+        sendQueue: this.logUploader.sendQueue,
       });
       this.logBuffer = [];
       this.logUploader.resetSendQueue();
     });
   }
 
-  addLogToSend (logLevel: LogLevel, message: string, details?: any): void {
+  addLogToSend(logLevel: LogLevel, message: string, details?: any): void {
     if (!this.isInitialized) {
       return;
     }
@@ -62,12 +74,16 @@ export class ServerLogger {
 
       /* newTrace will be `null` if the truncated trace was still too big */
       if (newTrace === null) {
-        this.logger.error('truncated message is too large to send to server. not sending message', {
-          originalTrace: trace,
-          originalTraceSize: calculateLogMessageSize(trace),
-          truncatedTrace: newTrace,
-          truncatedTraceSize: calculateLogMessageSize(newTrace)
-        }, { skipServer: true });
+        this.logger.error(
+          "truncated message is too large to send to server. not sending message",
+          {
+            originalTrace: trace,
+            originalTraceSize: calculateLogMessageSize(trace),
+            truncatedTrace: newTrace,
+            truncatedTraceSize: calculateLogMessageSize(newTrace),
+          },
+          { skipServer: true }
+        );
         return;
       }
 
@@ -83,7 +99,7 @@ export class ServerLogger {
     if (useNewBufferItem) {
       bufferItem = {
         size: 0,
-        traces: []
+        traces: [],
       };
     } else {
       bufferItem = this.logBuffer[this.logBuffer.length - 1];
@@ -92,21 +108,23 @@ export class ServerLogger {
     /* if pushing our trace onto the buffer item will be too large, we need a new buffer item */
     const exceedsMaxLogSize = bufferItem.size + traceMessageSize > MAX_LOG_SIZE;
     if (exceedsMaxLogSize) {
-      this.debug('`exceedsMaxLogSize` was `true`', {
+      this.debug("`exceedsMaxLogSize` was `true`", {
         logBuffer: this.logBuffer,
         bufferItem,
         incomingTrace: trace,
         incomingTraceSize: traceMessageSize,
-        maxAllowedTraceSize: MAX_LOG_SIZE
+        maxAllowedTraceSize: MAX_LOG_SIZE,
       });
 
       this.logBuffer.push({
         size: traceMessageSize,
-        traces: [trace]
+        traces: [trace],
       });
 
       /* since we pushed a new item, we need to send immediately */
-      this.debug('calling sendLogsToServer(true)', { logBuffer: this.logBuffer });
+      this.debug("calling sendLogsToServer(true)", {
+        logBuffer: this.logBuffer,
+      });
       this.sendLogsToServer(true);
       return;
     }
@@ -117,44 +135,56 @@ export class ServerLogger {
 
     /* if we don't have anything in the buffer, that means we have to push this new item */
     if (useNewBufferItem) {
-      this.debug('`this.logBuffer` was empty. pushing new buffer item', { logBuffer: this.logBuffer, bufferItem });
+      this.debug("`this.logBuffer` was empty. pushing new buffer item", {
+        logBuffer: this.logBuffer,
+        bufferItem,
+      });
       this.logBuffer.push(bufferItem);
     }
 
     /* this will setup the debounce timer (if it is not already running) */
-    this.debug('calling sendLogsToServer()', { logBuffer: this.logBuffer });
+    this.debug("calling sendLogsToServer()", { logBuffer: this.logBuffer });
     this.sendLogsToServer();
   }
 
-  sendAllLogsInstantly (): Promise<any>[] {
+  sendAllLogsInstantly(): Promise<any>[] {
     /* don't want this to be async because this is called from the window 'unload' event */
     /* this will send any queued up requests */
-    return this.logUploader.sendEntireQueue()
-      .concat(
-        /* this will send any items in the buffer still */
-        this.logBuffer.map((item: ILogBufferItem) =>
-          this.logUploader.postLogsToEndpointInstantly(this.convertToRequestParams(item.traces.reverse()), { saveOnFailure: true })
+    return this.logUploader.sendEntireQueue().concat(
+      /* this will send any items in the buffer still */
+      this.logBuffer.map((item: ILogBufferItem) =>
+        this.logUploader.postLogsToEndpointInstantly(
+          this.convertToRequestParams(item.traces.reverse()),
+          { saveOnFailure: true }
         )
-      );
+      )
+    );
   }
 
-  private async sendLogsToServer (immediate = false): Promise<any> {
+  private async sendLogsToServer(immediate = false): Promise<any> {
     if (!this.logBuffer.length) {
       /* clear timer */
       clearTimeout(this.debounceTimer);
       this.debounceTimer = null;
-      this.debug('buffer empty, not sending http request');
+      this.debug("buffer empty, not sending http request");
       return;
     }
 
     /* if we aren't sending immediately, then setup the timer */
     if (!immediate) {
       if (!this.debounceTimer) {
-        this.debug(`sendLogsToServer() 'immediate' is false. setting up 'debounceTimer' to ${this.debounceLogUploadTime}ms`);
+        this.debug(
+          `sendLogsToServer() 'immediate' is false. setting up 'debounceTimer' to ${this.debounceLogUploadTime}ms`
+        );
         /* eslint-disable-next-line @typescript-eslint/no-misused-promises */
-        this.debounceTimer = setTimeout(() => this.sendLogsToServer(true), this.debounceLogUploadTime);
+        this.debounceTimer = setTimeout(
+          () => this.sendLogsToServer(true),
+          this.debounceLogUploadTime
+        );
       } else {
-        this.debug(`sendLogsToServer() 'immediate' is false. 'debounceTimer' is already running`);
+        this.debug(
+          `sendLogsToServer() 'immediate' is false. 'debounceTimer' is already running`
+        );
       }
 
       return;
@@ -169,16 +199,25 @@ export class ServerLogger {
 
     try {
       // this.pendingHttpRequest = true;
-      this.debug('calling logUploader.postLogsToEndpoint() with', { bufferItem, newLogBuffer: this.logBuffer });
-      await this.logUploader.postLogsToEndpoint(this.convertToRequestParams(bufferItem.traces.reverse()));
+      this.debug("calling logUploader.postLogsToEndpoint() with", {
+        bufferItem,
+        newLogBuffer: this.logBuffer,
+      });
+      await this.logUploader.postLogsToEndpoint(
+        this.convertToRequestParams(bufferItem.traces.reverse())
+      );
     } catch (err) {
-      this.logger.emit('onError', err);
-      this.logger.error('Error sending logs to server', err, { skipServer: true });
+      this.logger.emit("onError", err);
+      this.logger.error("Error sending logs to server", err, {
+        skipServer: true,
+      });
       /* no-op: the uploader will attempt reties. if the uploader throws, it means this log isn't going to make to the server */
-      const statusCode = `${(err as any)?.status}` as '401' | '404';
+      const statusCode = `${(err as any)?.status}` as "401" | "404";
 
-      if (['401', '404'].includes(statusCode)) {
-        this.debug(`received a ${statusCode} from logUploader. stopping logging to server`);
+      if (["401", "404"].includes(statusCode)) {
+        this.debug(
+          `received a ${statusCode} from logUploader. stopping logging to server`
+        );
         this.logger.stopServerLogging(statusCode);
       }
     } finally {
@@ -187,12 +226,14 @@ export class ServerLogger {
     }
   }
 
-  private truncateLog (logLevel: LogLevel, log: ILogMessage): ITrace | null {
+  private truncateLog(logLevel: LogLevel, log: ILogMessage): ITrace | null {
     let trace: ITrace;
     let truncatedTraceSize: number;
-    const originalTraceSize = calculateLogMessageSize(this.convertToTrace(logLevel, log));
+    const originalTraceSize = calculateLogMessageSize(
+      this.convertToTrace(logLevel, log)
+    );
     const logCopy = deepClone(log);
-    const truncText = '[[TRUNCATED]]';
+    const truncText = "[[TRUNCATED]]";
 
     /* first truncate the details */
     logCopy.details = truncText;
@@ -200,13 +241,17 @@ export class ServerLogger {
     truncatedTraceSize = calculateLogMessageSize(trace);
 
     if (truncatedTraceSize <= MAX_LOG_SIZE) {
-      this.logger.warn('message too large to send to server. truncated log details', {
-        originalLog: log,
-        truncatedLog: logCopy,
-        originalTraceSize,
-        truncatedTraceSize,
-        maxAllowedTraceSize: MAX_LOG_SIZE
-      }, { skipServer: true });
+      this.logger.warn(
+        "message too large to send to server. truncated log details",
+        {
+          originalLog: log,
+          truncatedLog: logCopy,
+          originalTraceSize,
+          truncatedTraceSize,
+          maxAllowedTraceSize: MAX_LOG_SIZE,
+        },
+        { skipServer: true }
+      );
       return trace;
     }
 
@@ -216,13 +261,17 @@ export class ServerLogger {
     truncatedTraceSize = calculateLogMessageSize(trace);
 
     if (truncatedTraceSize <= MAX_LOG_SIZE) {
-      this.logger.warn('message too large to send to server. truncated log details & log message', {
-        originalLog: log,
-        truncatedLog: logCopy,
-        originalTraceSize,
-        truncatedTraceSize,
-        maxAllowedTraceSize: MAX_LOG_SIZE
-      }, { skipServer: true });
+      this.logger.warn(
+        "message too large to send to server. truncated log details & log message",
+        {
+          originalLog: log,
+          truncatedLog: logCopy,
+          originalTraceSize,
+          truncatedTraceSize,
+          maxAllowedTraceSize: MAX_LOG_SIZE,
+        },
+        { skipServer: true }
+      );
       return trace;
     }
 
@@ -230,12 +279,12 @@ export class ServerLogger {
     return null;
   }
 
-  private convertToLogMessage (message: string, details?: any): ILogMessage {
+  private convertToLogMessage(message: string, details?: any): ILogMessage {
     const log: ILogMessage = {
       clientTime: new Date().toISOString(),
       clientId: this.logger.clientId,
       message,
-      details
+      details,
     };
 
     const { originAppName, originAppVersion, originAppId } = this.logger.config;
@@ -249,31 +298,35 @@ export class ServerLogger {
     return log;
   }
 
-  private convertToTrace (level: LogLevel, log: ILogMessage): ITrace {
+  private convertToTrace(level: LogLevel, log: ILogMessage): ITrace {
     return {
       topic: this.logger.config.appName,
       level: level.toUpperCase(),
-      message: stringify(log)
+      message: stringify(log),
     };
   }
 
-  private convertToRequestParams (traces: ITrace[]): ISendLogRequest {
+  private convertToRequestParams(traces: ITrace[]): ISendLogRequest {
     return {
       accessToken: this.logger.config.accessToken,
       app: {
         appId: this.logger.config.appName,
-        appVersion: this.logger.config.appVersion
+        appVersion: this.logger.config.appVersion,
       },
-      traces
-    }
+      traces,
+    };
   }
 
-  private debug (message: string, details?: any): void {
+  private debug(message: string, details?: any): void {
     if (!this.logger.config.debugMode) {
       return;
     }
 
     /* tslint:disable-next-line:no-console */
-    console.log(`%c [DEBUG:${this.logger.config.appName}] ${message}`, 'color: #32a852', deepClone(details));
+    console.log(
+      `%c [DEBUG:${this.logger.config.appName}] ${message}`,
+      "color: #32a852",
+      deepClone(details)
+    );
   }
 }
